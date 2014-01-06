2736c94087be0b9aa170e1ae7ae4254f0a70a3ef
Jeffrey Smets <jeffrey.smets@gmail.com>
2014-01-06 16:27:04 +0100

    Added select duration screen Added profile screen

---
+++ CeMeOiOS/CeMeOiOS/SelectDurationNewMeetingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectDurationNewMeetingViewController.h
+//  CeMeOiOS
+//
+//  Created by Jeffrey Smets on 06/01/14.
+//  Copyright (c) 2014 Cegeka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectDurationNewMeetingViewController : UIViewController
+
+@end
